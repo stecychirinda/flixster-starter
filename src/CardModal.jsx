8cedfa4ad7c33751d0a9c1b081fedc52bf89f17a
Stecy Chirinda <schirinda@meta.com>
2025-06-11 16:34:44 -0700

    - I added the like feature. I added the watched feature. I used tenary condition to change between states.

--- src/CardModal.jsx
+++ src/CardModal.jsx
@@ -1,4 +1,4 @@
-import './Modal.css'
+import './CardModal.css'
 
 const CardModal = ({movie,onClose})=>{
     return (
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,15 +1,30 @@
 import "./MovieCard.css"
+import {useState} from "react"
 
 function MovieCard({ title, url, rating, onClick}) {
-    // const formattedRating = rating.toFixed(2);
+
+    const [liked,setLiked] = useState(false)
+    const [watched,setWatched] = useState(false)
+
+    const handleLike = (e) => {
+        e.stopPropagation()
+        setLiked(!liked)}
+
+    const handleWatched = (e) => {
+        e.stopPropagation()
+        setWatched(!watched)}
+
+    const formattedRating = rating.toFixed(2);
     return (
         <div className="movie" onClick={onClick}>
             <img className="img" src={`https://image.tmdb.org/t/p/w500${url}`} alt={title} />
             <div className="movie-info">
                 <h3>{title}</h3>
                 <span className={`tag ${rating >= 8 ? "green" : "orange"}`}>
-                    {rating}
+                    {formattedRating}
                 </span>
+                <button className="like" onClick={handleLike}>{liked?"❤️":"🤍"}</button>
+                <button className="watched" onClick={handleWatched}>{watched?"👀":"👁"}</button>
             </div>
         </div>)
 }
